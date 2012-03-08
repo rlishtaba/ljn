7de188d3c952c974c5227410cd5b10979c6fba4e
Leo Jay <leo@leo-OptiPlex-980.(none)>
2012-03-08 16:28:53 +0800

    add delete category

--- ljn/ui/component/CategoryList.py
+++ ljn/ui/component/CategoryList.py
@@ -1,5 +1,5 @@
 #coding:utf8
-from PyQt4.QtGui import QListWidget, QListWidgetItem, QAction, QInputDialog
+from PyQt4.QtGui import QListWidget, QListWidgetItem, QAction, QInputDialog, QMessageBox
 from ljn.Model import Category
 from ljn.Repository import get_session
 
@@ -16,6 +16,7 @@ class CategoryList(QListWidget):
 
         self.update_categories()
         self.addAction(self._create_rename_action())
+        self.addAction(self._create_delete_action())
 
     def _create_rename_action(self):
         a = QAction("Rename", self)
@@ -23,18 +24,24 @@ class CategoryList(QListWidget):
         a.triggered.connect(self._rename_category)
         return a
 
+    def _create_delete_action(self):
+        a = QAction("Delete", self)
+        a.setShortcut("Del")
+        a.triggered.connect(self._del_category)
+        return a
+
     def update_categories(self):
         self.clear()
         for c in Category.all(get_session()):
             self.addItem(CategoryItem(c))
 
-    def _rename_category(self, *args):
+    def _rename_category(self):
         items = self.selectedItems()
         if not items:
             return
 
         category = items[0].category
-        text, result = QInputDialog.getText(self, 'Rename', 'New category name:', text=category.name)
+        text, result = QInputDialog.getText(self, 'Rename category', 'New category name:', text=category.name)
         if not result:
             return
 
@@ -45,5 +52,21 @@ class CategoryList(QListWidget):
         s = get_session()
         c = Category.find_by_id(s, category.id)
         c.name = text
+        s.commit()
+        self.update_categories()
+
+    def _del_category(self):
+        items = self.selectedItems()
+        if not items:
+            return
+
+        category = items[0].category
+        msg = 'Delete "%s"?' % category.name
+        btn = QMessageBox.question(self, 'Delete category', msg, QMessageBox.Yes | QMessageBox.No)
+        if btn == QMessageBox.No:
+            return
+
+        s = get_session()
+        s.delete(Category.find_by_id(s, category.id))
         s.commit()
         self.update_categories()
