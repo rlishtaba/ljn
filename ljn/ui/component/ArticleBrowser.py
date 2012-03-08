72cfc172a167ba1bf7be36f26138c13707f62bc4
Leo Jay <leo@leo-OptiPlex-980.(none)>
2012-03-08 16:05:27 +0800

    add stylesheet

--- ljn/ui/component/ArticleBrowser.py
+++ ljn/ui/component/ArticleBrowser.py
@@ -1,9 +1,21 @@
 #coding:utf8
 from PyQt4.QtGui import QTextEdit
 
+STYLE_SHEET = r'''
+QTextEdit {
+    font-family: Cambria,"Hoefler Text",Georgia,"Times New Roman",serif;
+    font-size: 22px;
+    font-weight: 400;
+    color: #3D3936;
+    background-color: #F4EED9;
+}
+'''
+
 class ArticleBrowser(QTextEdit):
     def __init__(self, parent):
         QTextEdit.__init__(self, parent)
+
+        self.setStyleSheet(STYLE_SHEET)
 
     def set_article(self, article):
         self.setText(article.content)
